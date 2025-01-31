225ace77078deadf5fc684b23972b4bb80897c2e
Samarpan Koirala <samarpankoirala@gmail.com>
2025-01-31 11:51:37 -0600

    Added views and partials and 404 page to handle exceptions

--- src/app.js
+++ src/app.js
@@ -1,18 +1,61 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "../public")));
+//Define path for express config
+const viewPath = path.join(__dirname, "../template/views");
+const publicDirPath = path.join(__dirname, "../public");
+const partialDirPath = path.join(__dirname, "../template/partials");
 
-app.use(express.static(path.join(__dirname, "../public")));
+//Setup handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewPath);
 
-app.use(express.static(path.join(__dirname, "../public")));
+//Setup static directory to serve
+app.use(express.static(publicDirPath));
+
+hbs.registerPartials(partialDirPath);
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Samarpan Koirala",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Samarpan Koirala",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", { title: "Help", name: "Samarpan Koirala" });
+});
 
 app.get("/weather", (req, res) => {
   res.send({
     location: "Monroe, Louisiana",
     forecast: "Mostly sunny with a high of 85°F",
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Samarpan Koirala",
+    errorMessage: "ARTICLE PAGE NOT FOUND",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Samarpan Koirala",
+    errorMessage: "Page not found",
   });
 });
 
